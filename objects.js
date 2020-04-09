399a6522ecbfc9c04d3a5805c413de50b31efbf5
kiku ishimoto <kikuish@gmail.com>
2020-04-09 04:12:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   indie: 'peach face'
 }
 
-funtion updatePlaylist(playlist, artist, song) {
+function updatePlaylist(playlist, artist, song) {
   playlist.artist = song;
   return playlist
 
